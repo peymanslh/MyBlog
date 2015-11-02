19232926815a3403d0c45fcc0a84f2c43dd7c876
Peyman Salehi <peyman_salehi@icloud.com>
2015-11-02 12:13:19 +0330

    Change project style

--- layouts/query-project.php
+++ layouts/query-project.php
@@ -2,17 +2,19 @@
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
 
-            <div class="col-md-6 project-thumb">
-                <a href="<?php the_permalink() ?>">
-                    <?php if (has_post_thumbnail()) {
-                        the_post_thumbnail('project_thumb');
-                    } else {
-                        echo '<img src="' . esc_url(get_template_directory_uri()) . '/assets/images/no-thumbnail.png" />';
-                    } ?>
+            <div class="col-md-6">
+                <div class="project-thumb">
+                    <a href="<?php the_permalink() ?>">
+                        <?php if (has_post_thumbnail()) {
+                            the_post_thumbnail('project_thumb');
+                        } else {
+                            echo '<img src="' . esc_url(get_template_directory_uri()) . '/assets/images/no-thumbnail.png" />';
+                        } ?>
 
-                    <div class="overaly"></div>
-                    <div class="title"><span><?php the_title(); ?></span></div>
-                </a>
+                        <div class="overaly"></div>
+                        <div class="title"><span><?php the_title(); ?></span></div>
+                    </a>
+                </div>
             </div>
         <?php endwhile; ?>
     <?php endif; ?>
