1c47dcbd24ec599379b8c47f86bc061c226b1fed
Peyman Salehi <peyman_salehi@icloud.com>
2015-11-13 20:32:45 +0330

    Remove wp_debug

--- functions.php
+++ functions.php
@@ -144,16 +144,3 @@ function cd_add_editor_styles()
 {
     add_editor_style(esc_url(get_template_directory_uri()) . '/assets/css/main.css');
 }
-
-
-//Enable WP_DEBUG mode
-define('WP_DEBUG', true);
-// Enable Debug logging to the /wp-content/debug.log file
-define('WP_DEBUG_LOG', true);
-
-// Disable display of errors and warnings
-define('WP_DEBUG_DISPLAY', true);
-@ini_set('display_errors',0);
-
-// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
-define('SCRIPT_DEBUG', true);
